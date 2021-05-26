3f943ec7448548377e8360baa47a992af8209655
Byron Warner <bfwarner@gmail.com>
2021-05-26 09:08:53 -0700

    Switched back to sendWithTemplate

--- pages/api/signature.js
+++ pages/api/signature.js
@@ -28,10 +28,11 @@ export default async function handler(req, res) {
         signers,
       };
       try {
-      const result = await hellosign.signatureRequest.createEmbeddedWithTemplate(opts);
+      // const result = await hellosign.signatureRequest.createEmbeddedWithTemplate(opts);
+      const result = await hellosign.signatureRequest.sendWithTemplate(opts);
       console.log('createEmbeddedWithTemplate result  ', result);
-      const { signature_request: { signatures } } = result;
-      const { embedded: { sign_url: signingUrl } } = await hellosign.embedded.getSignUrl(signatures[0]);
+      const { signature_request: { signing_url: signingUrl, signatures } } = result;
+      // const { embedded: { sign_url: signingUrl } } = await hellosign.embedded.getSignUrl(signatures[0]);
       console.log(`The signing url is: ${signingUrl} signatures ${signatures}`);
         res.status(200).json({ data: { signingUrl, signatures } });
       } catch (err) {
