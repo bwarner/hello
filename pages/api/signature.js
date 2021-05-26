dbd1d7c7046b4316ef6af4942e53d48c73209e8c
Byron Warner <bfwarner@gmail.com>
2021-05-26 01:17:40 -0700

    Calls HelloSign API

---
+++ pages/api/signature.js
@@ -0,0 +1,47 @@
+
+import HellosignSDK from 'hellosign-sdk';
+
+const clientId = process.env.NEXT_PUBLIC_HELLOSIGN_CLIENT_ID;
+const hellosign = HellosignSDK({ key: process.env.API_KEY });
+export default async function handler(req, res) {
+  const {
+    body,
+    method,
+  } = req
+  switch (method) {
+    case 'POST': {
+      const {
+        signers,
+        clientId,
+        template_id,
+        subject,
+        message,
+      } = JSON.parse(body);
+
+      // Update or create data in your database
+      const opts = {
+        test_mode: 1,
+        clientId,
+        template_id,
+        subject,
+        message,
+        signers,
+      };
+      try {
+      const result = await hellosign.signatureRequest.sendWithTemplate(opts);
+      console.log('sendTemplate result  ', result);
+      const { signature_request: { signing_url: signingUrl, signatures } } = result;
+      // const { embedded: { sign_url: signUrl } } = await hellosign.embedded.getSignUrl(signatures[0]);
+      console.log(`The signing url is: ${signingUrl} signatures ${signatures}`);
+        res.status(200).json({ data: { signingUrl, signatures } });
+      } catch (err) {
+        console.error(err);
+        res.status(400).json({ error: err.message ? err.message : err.toString()});
+      }
+      break
+    }
+    default:
+      res.setHeader('Allow', ['POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,122 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useCallback, useState } from 'react';
+
+const templateId="e17602c9261a29ede9fe60bfb35b1e71ea22691f";
 
 export default function Home() {
+  const [subject, setSubject] = useState("Bill Of Lading");
+  const [message, setMessage] = useState("Enjoy your Move");
+  const [name, setName] = useState("Byron");
+  const [email, setEmail] = useState("bwarner@oncue.co");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onSubmit = useCallback(async (event) => {
+    event.preventDefault();
+
+    const HelloSign = (await import('hellosign-embedded')).default;
+
+    const { target } = event;
+
+    const opts = {
+      test_mode: 1,
+      clientId: process.env.NEXT_PUBLIC_HELLOSIGN_CLIENT_ID,
+      template_id: templateId,
+      subject,
+      message,
+      signers: [
+        {
+          email_address: email,
+          name,
+          role: 'Client',
+        },
+      ],
+    };
+    const client = new HelloSign();
+    try {
+      const res = await fetch('/api/signature', {
+        method: 'POST',
+        body: JSON.stringify(opts),
+      });
+
+      const body = await res.json();
+      if (res.ok) {
+        const { data: { signingUrl } } = body;
+        debugger;
+        client.open(signingUrl, {
+          clientId: process.env.NEXT_PUBLIC_HELLOSIGN_CLIENT_ID,
+        });
+      } else {
+        const { error } = body;
+        console.error(error);
+        setErrorMessage(error);
+      }
+    } catch (err) {
+      console.error(err);
+      setErrorMessage(err.message ? err.message : err.toString());
+    }
+  }, [subject, message, name, email]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>HelloSign Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <h1 className={styles.title}>
+            Testing Embedding Signing
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <div>
+            {errorMessage && <span>{errorMessage}</span>}
+          </div>
+          <form className="my-form" onSubmit={onSubmit}>
+            <div className="form-input">
+            <label htmlFor="subject">
+              Subject
+              <input
+              name="subject"
+              id="subject"
+              type="text"
+              value={subject}
+              required
+              onChange={event => setSubject(event.target.value)}
+              />
+            </label>
+            </div>
+            <div className="form-input"><label htmlFor="message">
+              Message
+              <input
+               name="message"
+              id="message"
+              type="text"
+              required
+              value={message}
+              onChange={event => setMessage(event.target.value)}
+              />
+            </label>
+            </div>
+            <div className="form-input"><label htmlFor="name">
+              Name
+              <input name="name"
+                id="name"
+                type="text"
+                required
+                value={name}
+                onChange={event => setName(event.target.value)}
+              />
+            </label>
+            </div>
+            <div className="form-input">
+            <label htmlFor="email">
+              Email
+              <input required name="email" type="email"  id="email" value={email} onChange={event => setEmail(event.target.value)} />
+            </label>
+            </div>
+            <div className="form-input">
+              <button type="submit">Create</button>
+            </div>
+          </form>
       </main>
 
       <footer className={styles.footer}>
