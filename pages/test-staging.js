44f169397b56682cef4d0165c69c949c5e9144bb
Byron Warner <bfwarner@gmail.com>
2022-01-12 16:55:52 -0800

    fixed test staging page

--- pages/test-staging.js
+++ pages/test-staging.js
@@ -8,11 +8,11 @@ export default function Test() {
     </iframe>
 
     <h1>Long Webform</h1>
-    <iframe src="https://localhost:3000/ks-moving-packing?iframe=1&mover_slug=ks-moving-packing" width="600" height="400">
+    <iframe src="https://oncueco-staging.herokuapp.com/ks-moving-packing?iframe=1&mover_slug=ks-moving-packing" width="600" height="400">
 
     </iframe>
     <h1>Blocked site (Expected)</h1>
-    <iframe src="https://localhost:3000/admin" width="600" height="400">
+    <iframe src="https://oncueco-staging.herokuapp.com/admin" width="600" height="400">
 
     </iframe>
     </>
